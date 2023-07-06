e86d0ba25f5887ce4018cbabe4be399e57bbe4ef
chencc <jery1997@foxmail.com>
2023-07-06 15:31:49 +0800

    chore: 去除局域网配置

--- test/normal.vue
+++ test/normal.vue
@@ -4,9 +4,7 @@
     <h1 @click="add" style="cursor: pointer">我是外层项目， 点我props+1 {{test}}</h1>
     <div style="display: flex">
       <UseMaterial :src="src" :test="test" ref="useMaterial" />
-      <!-- <UseMaterial :src="'http://10.28.184.222:8981/static/material/components/1539525839491223554/1.0.0'" :test="test" ref="useMaterial2" />
-      <UseMaterial :src="src" :test="test" ref="useMaterial" />
-      <use-material :src="src2" :test="test" ref="useMaterial" /> -->
+      <!-- <UseMaterial :src="src" :test="test" ref="useMaterial" /> -->
     </div>
   </div>
 </template>
@@ -22,8 +20,7 @@ export default {
   data () {
     return {
       test: 0,
-      src: "http://10.28.184.222:8981/static/material/blocks/1630018384402415618/1.0.0",
-      src2: "http://10.28.184.222:8981/static/material/components/1539166635282714626/1.1.1"
+      src: "./test.zip",
     }
   },
   mounted() {
@@ -31,7 +28,6 @@ export default {
   },
   methods: {
     changeSrc() {
-      this.src = this.src === "http://10.28.184.222:8981/static/material/components/1539416291669495809/1.1.2" ? "http://10.28.184.222:8981/static/material/components/1543783561338400770/1.0.0" : "http://10.28.184.222:8981/static/material/components/1539416291669495809/1.1.2" 
       console.log(this.src)
     },
     add() {
--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,4 @@ module.exports = {
   chainWebpack: config => {
       config.module.rules.delete('eslint');
   },
-  devServer: {
-      proxy: 'http://10.28.184.222:8981/'
-  }
 }
